dd72d96e4e5de8242d0d5f846f22ea93db760896
LMO GerritBot <gerrit@libremobileos.com>
2023-08-19 11:26:49 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rhode.mk
+    $(LOCAL_DIR)/lmodroid_rhode.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rhode-user \
-    lineage_rhode-userdebug \
-    lineage_rhode-eng
+    lmodroid_rhode-user \
+    lmodroid_rhode-userdebug \
+    lmodroid_rhode-eng
